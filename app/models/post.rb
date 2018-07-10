9470c45573c706b917a86b1f1b9543bcdaa8337b
Chiel <c.hackman@cevesvergeer.nl>
2018-07-10 08:43:00 +0200

    Post model tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+
+  scope :active_posts, -> { where(active: true) }
+  scope :inactive_posts, -> { where(active: false) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+    it "validates title presence" do
+      post = Post.new(body: 'New post body').save
+      expect(post).to eq(false)
+    end
+
+    it "validates body presence" do
+      post = Post.new(title: 'New post title').save
+      expect(post).to eq(false)
+    end
+
+    it "should save successfully" do
+      post = Post.new(title: 'New post title', body: 'New post body').save
+      expect(post).to eq(true)
+    end
+  end
+
+  context "scope tests" do
+    let (:params) { {title: 'Post title', body: 'Post body'} }
+    before(:each) do
+      Post.new(params).save
+      Post.new(params).save
+      Post.new(params.merge(active: true)).save
+    end
+
+    it "returns active posts" do
+      expect(Post.active_posts.size).to eq(1)
+    end
+
+    it "returns inactive posts" do
+      expect(Post.inactive_posts.size).to eq(2)
+    end
+  end
 end
